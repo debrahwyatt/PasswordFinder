60173663ee42cdf025f848bcd518b5ec56b014a5
Devlin Wyatt <devlinwyatt@gmail.com>
2017-07-16 16:34:59 -0700

    PasswordFinder.java
    This is the preliminary code. Although it works to find passwords that are three characters long the next version will need to include a smoother transitional format for adding String length to the programs guesses as it progresses.

---
+++ PasswordFinder.java
@@ -0,0 +1,65 @@
+/*
+The purpose of this program is to go through every combination of characters
+to find the secret word/string.
+ */
+package passwordfinder;
+
+/**
+ * @author Devlin Wyatt
+ */
+public class PasswordFinder {
+
+    public static void main(String[] args) {
+
+        String secretWord = "12p"; //This is the String to guess.
+        String programWord = ""; //This is the programs current String.
+
+        for (int i = 32; i < 126; i++) {
+
+            programWord = programWord + (char) i;
+            System.out.println(programWord);
+
+            if (programWord.equals(secretWord)) {
+                System.out.println("Password found: " + programWord);
+                return;
+
+            } else {
+                programWord = "";
+            }
+        }
+
+        for (int i = 32; i < 126; i++) {
+            for (int j = 32; j < 126; j++) {
+
+                programWord = programWord + (char) i + (char) j;
+                System.out.println(programWord);
+
+                if (programWord.equals(secretWord)) {
+                    System.out.println("Password found: " + programWord);
+                    return;
+
+                } else {
+                    programWord = "";
+                }
+            }
+        }
+
+        for (int i = 32; i < 126; i++) {
+            for (int j = 32; j < 126; j++) {
+                for (int k = 32; k < 126; k++) {
+
+                    programWord = programWord + (char) i + (char) j + (char) k;
+                    System.out.println(programWord);
+
+                    if (programWord.equals(secretWord)) {
+                        System.out.println("Password found: " + programWord);
+                        return;
+
+                    } else {
+                        programWord = "";
+                    }
+                }
+            }
+        }
+    }
+}
